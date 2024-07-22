ea127a1da35875d732d69c5300f48a9fa9aa56db
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-22 10:09:55 -0400

    chore: Update keybindings in KeyMap struct

--- libs/keymaps/keymaps.go
+++ libs/keymaps/keymaps.go
@@ -3,13 +3,11 @@ package keymaps
 import "github.com/charmbracelet/bubbles/key"
 
 type KeyMap struct {
-	Up    key.Binding
-	Down  key.Binding
-	Left  key.Binding
-	Right key.Binding
-	Enter key.Binding
-	Help  key.Binding
-	Quit  key.Binding
+	Tab      key.Binding
+	ShiftTab key.Binding
+	Enter    key.Binding
+	Help     key.Binding
+	Quit     key.Binding
 }
 
 // ShortHelp returns keybindings to be shown in the mini help view. It's part
@@ -21,7 +19,7 @@ func (k KeyMap) ShortHelp() []key.Binding {
 // FullHelp returns keybindings for the expanded help view. It's part of the
 // key.Map interface.
 func (k KeyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{{k.Help}, {k.Quit}, {k.Enter}}
+	return [][]key.Binding{{k.Help}, {k.Quit}, {k.Enter}, {k.Tab}, {k.ShiftTab}}
 }
 
 var Keys = KeyMap{
@@ -36,5 +34,13 @@ var Keys = KeyMap{
 	Enter: key.NewBinding(
 		key.WithKeys("enter"),
 		key.WithHelp("enter", "continue"),
+	),
+	Tab: key.NewBinding(
+		key.WithKeys("tab"),
+		key.WithHelp("tab", "switch tab"),
+	),
+	ShiftTab: key.NewBinding(
+		key.WithKeys("shift+tab"),
+		key.WithHelp("shift+tab", "switch tab backwards"),
 	),
 }
